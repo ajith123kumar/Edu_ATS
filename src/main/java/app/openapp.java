b5bd2c19745eeac7a3cdbe6c4ffae5c76eef5266
ajith123kumar <dajith1kumar@gmail.com>
2024-02-19 15:39:55 +0530

    test

--- src/main/java/app/openapp.java
+++ src/main/java/app/openapp.java
@@ -1,10 +1,20 @@
 package app;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class openapp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
 
+		WebDriver driver = new ChromeDriver();
+        
+	      //  WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+	        driver.get("http://www.google.com");
+	        WebElement element = driver.findElement(By.name("q"));
+	        Thread.sleep(6000);
 	}
 
 }
